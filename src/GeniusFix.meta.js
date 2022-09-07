f2fa2f756edf643a79f9eeaa7fb95c8c55b5d4b8
greatalazar <greatalazar@gmail.com>
2022-09-07 15:35:35 +0300

    Create GeniusFix.meta.js

---
+++ src/GeniusFix.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         	Genius: Back to The Original Page Layout
+// @namespace    	https://greatalazar.github.io
+// @version      	0.1
+// @description  	Automatically redirect to the old page layout, I have nothing but contempt for the "new genius page", With this we can go back to the old and cool page
+// @author       	greatalazar
+// @match        	https://genius.com/*lyrics
+// @match        	https://genius.com/*annotated
+// @icon         	https://www.google.com/s2/favicons?sz=64&domain=genius.com
+// ==/UserScript==
